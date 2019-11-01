84e6aa3fee2ae8c65e1c5903a597419e83290548
KamiMaki <41109762+KamiMaki@users.noreply.github.com>
2019-11-01 16:02:40 +0800

    add 以點易點bundle

--- app/src/main/java/com/dragon/blockchain_demo/Receipt.java
+++ app/src/main/java/com/dragon/blockchain_demo/Receipt.java
@@ -6,19 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Receipt extends AppCompatActivity {
     ImageButton bt;
+    TextView tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
         bt = findViewById(R.id.button);
+        tv1 = findViewById(R.id.textView55);
+        tv2 = findViewById(R.id.textView54);
         Toast.makeText(Receipt.this,"撮合成功!", Toast.LENGTH_SHORT).show();
+        Bundle bundle =this.getIntent().getExtras();
+
+        String[] A = bundle.getString("A").split(",");
+        String B = bundle.getString("B");
+        String[] amount = bundle.getString("amount").split(",");
+        tv1.setText(amount[0]);
+        tv2.setText(amount[1]);
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 startActivity(new Intent(Receipt.this, MainActivity.class));
             }
         });
